549d000a112cb36078dcf734e92d3f43d2a10fbf
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-16 14:31:07 -0800

    refactored code to take this.state.term from child component over to parent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,16 +4,17 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit = (event) => {
+  onFormSubmit(event) {
     event.preventDefault();
 
-    console.log(this.state.term);
-  };
+    // console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={this.onFormSubmit.bind(this)} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
