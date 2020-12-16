b1c7dbd304424715079f6e8f83b9aa04fdec3036
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-15 21:01:28 -0800

    added a new input change handler on line 15 of SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,17 @@ import React from "react";
 
 // knows to use class since you need to handle user input using state
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={(e) => console.log(e.target.value)} />
           </div>
         </form>
       </div>
